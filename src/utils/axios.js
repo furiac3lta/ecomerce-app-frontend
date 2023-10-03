ec4da46e5eb999cc0b8c25ea6606406150c122bc
Marcelo Colum <colummarcelo@gmail.com>
2023-10-03 11:01:59 -0300

    change axios url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import deafultAxios from 'axios';
 
 const axios = () => deafultAxios.create({
     //https://e-commerce-api-v2.academlo.tech/api/v1'
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://ecommerce-app-academlo.onrender.com',
     headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`
     }
